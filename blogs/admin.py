6724115b6f035397b4c0ade893037c2bd0c23f7c
lexStack3 <alexanderedim80@gmail.com>
2025-07-04 07:21:35 +0100

    Blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Category, Blog
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    # Prepopulation for slug
+    prepopulated_fields = {'slug': ('title',)}
+
+    # Formatting how Blog columns will be displayed on the admin page
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+
+    # Search fields
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+
+    # Add editable fields
+    list_editable = ('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
